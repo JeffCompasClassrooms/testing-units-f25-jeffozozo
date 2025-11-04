d042dd4487fd3b561eb00c17ecec9dc15857f703
jeffcompas <jeff.compas@utahtech.edu>
2025-11-04 11:20:30 -0700

    added workflows

--- test_calc.py
+++ test_calc.py
@@ -3,19 +3,22 @@ import pytest
 
 my_calc = Calc()
 
-def test_the_calc():
+def test_add_works_with_good_numbers():
 
 	assert my_calc.add(0,0) == 0
 	assert my_calc.add(1,1) == 2
 	assert my_calc.add(-1,1) == 0
 
+def test_mult_works_with_good_numbers():
 	assert my_calc.mul(0,0) == 0
 	assert my_calc.mul(1,1) == 1
 	assert my_calc.mul(-1,1) == -1
 
+def test_div_by_zero_throws_exception():    
 	with pytest.raises(ZeroDivisionError):
 		my_calc.div(1,0)
 
+def test_div_works_with_good_numbers():    
 	assert my_calc.div(10,5) == 2
 	assert my_calc.div(-1,-1) == 1
 
